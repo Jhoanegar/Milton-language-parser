70a702d348fbc94e8595ca3a1aa18b05abd8c714
unknown <jhoanegar@gmail.com>
2020-03-28 01:10:20 -0600

    Add logical operators grammar

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 const input = `#Init
 terminal when (Booting and not MiltonAllowed and not MiltonNotAllowed and
   (InTerminal_Ending_Gates or InTerminal_Ending_Crypt or InTerminal_Ending_Tower or
-   InTerminal_Nexus_Floor1 or InTerminal_Nexus_Floor2 or InTerminal_Nexus_Floor3 or InTerminal_Nexus_Floor4) )
+   InTerminal_Nexus_Floor1 or InTerminal_Nexus_Floor2 or InTerminal_Nexus_Floor3 or InTerminal_Nexus_Floor4) ) { notext
+  setlocal: MiltonNotAllowed
+  goto: Booting
+}
 `
 .replace(/\n[\t ]+/g, ' ')
 .replace(/\u0023.*\n/g, '')
@@ -15,7 +18,7 @@ terminal when (Booting and not MiltonAllowed and not MiltonNotAllowed and
 const empty = "";
 // Parse something!
 console.log(`\n===========\n${input}=============\n`);
-parser.feed("not VariableName and OtherVariable or OtherVariable");
+parser.feed(input);
 
 // parser.results is an array of possible parsings.
 console.log("LENGTH: " + parser.results.length); // [[[[ "foo" ],"\n" ]]]
